723e613b7aa3b1c4f90d70ec8c9d6416d399edf3
Paundra Yudha Dharmawan <155907525+paundrayudhad@users.noreply.github.com>
2025-06-18 22:36:01 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,6 @@ export default function Home() {
       <main className="flex-grow">
         <Hero />
         <Services />
-        <Portfolio />
         <Pricing />
         <Testimonials />
         <Contact />
